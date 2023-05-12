57475023d16d0fdf7c5f0db075886fefa2146881
@joselmosilva <joselmom@yahoo.com.br>
2023-05-12 14:47:04 -0300

    atualizacoes

---
+++ js/if else.js
@@ -0,0 +1,18 @@
+/* controle de fluxo - condicionais - IF  / ELSE 
+IF = se
+
+else = se nao 
+
+O(else) sempre vem junto com (IF) nunca sozinho
+
+*/
+ const age = 30
+ if(age >=20){
+    console.log('A pessoa e maior de idade')
+
+ } else{
+    console.log( 'A pessoa nao maior de idade')
+ }
+ console.log(' terminei de execultar meu codigo')
+
+
---
+++ js/tipos de dados -string.js
@@ -0,0 +1,14 @@
+
+let myfirsstring = " E aqui dentro eu posso escrever o que eu quizer!"
+let mySeccondstring = 'Agora com aspas simples'
+let mytrirstring = `com crase eu tambem posso pular linhas 
+a hora que eu 
+quiser`// template literals oun template strings
+
+let text = "Fulano de tal falou o seguinte: ' Eu aprendi js'"
+
+let numberOfPeopleInClass = 34 
+
+let  myTexWithSuperPowers = `Tinha exatamente ${10 + 20 * 4} pessoas na aula `
+
+console.log( myTexWithSuperPowers)
