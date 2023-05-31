d9ef1d98321e53335d3fe59c9c08896b4d88a782
@joselmosilva <joselmom@yahoo.com.br>
2023-05-31 09:38:27 -0300

    atualizar

--- projeto/script.js
+++ projeto/script.js
@@ -1,5 +1,34 @@
-const convertButton = document.querySelector(".convertbutton")
+const convertButton = document.querySelector(".convert-button")
 
-console.log("convertButton")
+function convertValues(){
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const CurrencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const CurrencyValueConverted = document.querySelector(".currency-value-converted")
 
 
+    const dolarToday = 5.2
+
+    const convertdValue = inputCurrencyValue / dolarToday
+
+
+    CurrencyValueToConvert. innerHTML = new Intl.NumberFormat("pt-BR", {
+        style:"currency",
+        currency:"BRL"
+    }).format(inputCurrencyValue)
+
+
+    CurrencyValueConverted. innerHTML = new Intl.NumberFormat("en-US", {
+        style:"currency",
+        currency:"USD"
+    }).format(convertdValue)
+
+
+    
+
+
+
+    
+}
+
+
+convertButton.addEventListener("click",convertValues)
